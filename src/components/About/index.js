7f9be9af18903be2986d8fb48005a7ce1f704721
HL <lecaoquochung@gmail.com>
2017-08-19 21:49:44 +0900

    Express & docker for reactjs app

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,18 +5,18 @@ import classnames from 'classnames';
 import './style.css';
 
 export default class About extends Component {
-  // static propTypes = {}
-  // static defaultProps = {}
-  // state = {}
+    // static propTypes = {}
+    // static defaultProps = {}
+    // state = {}
 
-  render() {
-    const { className, ...props } = this.props;
-    return (
-      <div className={classnames('About', className)} {...props}>
-        <h1>
-          About
+    render() {
+        const { className, ...props } = this.props;
+        return (
+            <div className={classnames('About', className)} {...props}>
+                <h1>
+                    About
         </h1>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,24 +6,24 @@ import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
-  // static propTypes = {}
-  // static defaultProps = {}
-  // state = {}
+    // static propTypes = {}
+    // static defaultProps = {}
+    // state = {}
 
-  render() {
-    const { className, ...props } = this.props;
-    return (
-      <div className={classnames('App', className)} {...props}>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    render() {
+        const { className, ...props } = this.props;
+        return (
+            <div className={classnames('App', className)} {...props}>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
